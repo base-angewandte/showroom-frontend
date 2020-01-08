7e87dec612b3d8dfda0450116c742a9db087d462
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2020-01-08 10:41:19 +0100

    fix: adding vue-server-renderer to dependencies temporarily to avoid version 2.6.11 (component native modifier issue)

--- pages/_lang/discover.vue
+++ pages/_lang/discover.vue
@@ -6,11 +6,22 @@
     <h2 class="subtitle">
       {{ $t('showcase') }}
     </h2>
+    <BaseImageBox
+      :entry-id="12"
+      :total="10"
+      :show="true"
+      :tabs="['de', 'en']"
+      label="test" />
   </div>
 </template>
 
 <script>
+import { BaseImageBox } from 'base-ui-components';
+
 export default {
+  components: {
+    BaseImageBox,
+  },
 };
 </script>
 
