0c9d9f404a97905233bc904c9884aa38aef2783c
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2021-05-17 17:27:08 +0200

    feat: #1371: add primary_details box

--- components/Detail.vue
+++ components/Detail.vue
@@ -2,9 +2,64 @@
   <div class="base-sr-detail">
     <div
       class="base-sr-head">
-      <div class="base-sr-row base-sr-head__primary">
-        primary_details
-      </div>
+      <BaseExpandBox
+        :show-more-text="$t('show_more')"
+        :show-less-text="$t('show_less')"
+        padding="large"
+        class="base-sr-row base-sr-head__primary">
+        <!-- title -->
+        <h1 class="base-sr-h1">
+          {{ data.title }}
+        </h1>
+
+        <!-- subtext -->
+        <BaseTextList
+          v-if="data.subtext"
+          :data="[{ data: data.subtext }]" />
+
+        <!-- expertise / type -->
+        <!-- TODO: extract to component -->
+        <template
+          v-if="data.expertise">
+          <h2
+            v-if="type === 'person'"
+            class="base-sr--mt-small">
+            {{ $t('expertise') }}
+          </h2>
+
+          <div
+            class="base-sr-chips">
+            <template
+              v-if="type === 'person'">
+              <span
+                v-for="item in data.expertise"
+                :key="item"
+                class="base-sr-chips__item">{{ item }}</span>
+            </template>
+            <template v-else>
+              <span class="base-sr-chips__item">{{ data.type }}</span>
+            </template>
+          </div>
+        </template>
+
+        <!-- primary details -->
+        <BaseTextList
+          render-label-as="h2"
+          :data="data.primary_details"
+          class="base-sr-head__text-list" />
+
+        <!-- action links: eg. print, sharing, subscribe -->
+        <template v-slot:footer>
+          <BaseButton
+            :has-background-color="false"
+            :text="$i18n.t('print')"
+            :icon-colored="true"
+            icon="print"
+            icon-position="top"
+            icon-size="large"
+            style="padding-left: 0;" />
+        </template>
+      </BaseExpandBox>
 
       <!-- secondary details -->
       <div
@@ -22,6 +77,20 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import {
+  BaseButton,
+  BaseExpandBox,
+  BaseTextList,
+} from 'base-ui-components';
+
+import 'base-ui-components/dist/components/BaseButton/BaseButton.css';
+import 'base-ui-components/dist/components/BaseExpandBox/BaseExpandBox.css';
+import 'base-ui-components/dist/components/BaseTextList/BaseTextList.css';
+
+Vue.use(BaseButton);
+Vue.use(BaseExpandBox);
+Vue.use(BaseTextList);
 export default {
   name: 'Detail',
   props: {
@@ -79,6 +148,22 @@ export default {
       @media screen and (min-width: $breakpoint-small) {
         width: 33.33%;
       }
+    }
+
+    &__text-list {
+      margin-top: $line-height;
+    }
+  }
+
+  /* chips */
+  .base-sr-chips {
+    display: flex;
+
+    &__item {
+      padding: 0 $spacing-small;
+      margin-right: $spacing-small;
+      background-color: $background-color;
+      text-transform: capitalize;
     }
   }
 </style>
