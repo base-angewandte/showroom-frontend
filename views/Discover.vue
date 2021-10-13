73288a35ba4f5639b7165c4c0e5e1ffbb4352d02
Sandra Hauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2021-10-13 14:41:10 +0200

    fix: remove hardcoded carousel data and fix error in offset calculation if offset undefined

--- views/Discover.vue
+++ views/Discover.vue
@@ -9,7 +9,7 @@
       <transition
         name="fade">
         <Showcase
-          v-if="isInitialView"
+          v-if="isInitialView && carouselData && carouselData.length"
           :data="carousel" />
       </transition>
     </client-only>
@@ -41,11 +41,12 @@ export default {
     const response = await $api.public.api_v1_search_create({}, {
       requestBody: {
         filters: parsedFilters,
-        offset: (page - 1) * entryNumber,
+        offset: (page ? (Number(page) - 1) : 0) * entryNumber,
         limit: entryNumber,
       },
     });
     const searchResults = JSON.parse(response.data);
+    // TODO: get carousel data (either separate request or together with initial search data)
     return { searchResults, appliedFilters: parsedFilters };
   },
   data() {
@@ -53,129 +54,7 @@ export default {
       isInitialView: true,
       searchResults: [],
       appliedFilters: [],
-      carousel: [
-        {
-          uid: '1',
-          title: 'Whatever Works Best For You',
-          subtext: 'On the subject of labor',
-          description: 'Exhibition',
-          additional: '07.05.2020 - 21.05.2020',
-          // eslint-disable-next-line global-require
-          imageUrl: 'https://placeimg.com/460/341/arch',
-          href: 'www.angewandte.at',
-          previews: [
-            {
-              '460w': 'https://placeimg.com/460/341/animal',
-            },
-            {
-              '640w': 'https://placeimg.com/640/480/animal',
-            },
-            {
-              '768w': 'https://placeimg.com/768/576/animal',
-            },
-          ],
-        },
-        {
-          uid: '2',
-          title: 'Throwing Gestures',
-          subtext: 'Make The Unknown Visible',
-          description: 'Performance',
-          additional: '07.05.2020 - 21.05.2020',
-          imageUrl: 'https://placeimg.com/640/480/tech',
-          href: 'http://base.uni-ak.ac.at',
-          previews: [
-            {
-              '460w': 'https://placeimg.com/460/341/tech',
-            },
-            {
-              '640w': 'https://placeimg.com/640/480/tech',
-            },
-            {
-              '768w': 'https://placeimg.com/768/576/tech',
-            },
-          ],
-        },
-        {
-          uid: '3',
-          title: 'Move From Left to Right and Back and Then Turn Around',
-          subtext: 'Moving Frames and Other Variables',
-          description: 'Video Installation',
-          additional: 'Florian Bettel, Max Arheimer',
-          imageUrl: 'https://placeimg.com/640/480/nature',
-          href: 'http://base.uni-ak.ac.at',
-          previews: [
-            {
-              '460w': 'https://placeimg.com/460/341/nature',
-            },
-            {
-              '640w': 'https://placeimg.com/640/480/nature',
-            },
-            {
-              '768w': 'https://placeimg.com/768/576/nature',
-            },
-          ],
-        },
-        {
-          uid: '4',
-          title: 'Title',
-          subtext: 'Subtitle',
-          description: 'Ausstellung 4',
-          additional: '07.05.2020 - 21.05.2020',
-          imageUrl: 'https://placeimg.com/640/480/animal',
-          href: 'http://base.uni-ak.ac.at',
-          previews: [
-            {
-              '460w': 'https://placeimg.com/460/341/animal',
-            },
-            {
-              '640w': 'https://placeimg.com/640/480/animal',
-            },
-            {
-              '768w': 'https://placeimg.com/768/576/animal',
-            },
-          ],
-        },
-        {
-          uid: '5',
-          title: 'Title',
-          subtext: 'Subtitle',
-          description: 'Ausstellung',
-          additional: '07.05.2020 - 21.05.2020',
-          imageUrl: 'https://placeimg.com/640/480/people',
-          href: 'http://base.uni-ak.ac.at',
-          previews: [
-            {
-              '460w': 'https://placeimg.com/460/341/people',
-            },
-            {
-              '640w': 'https://placeimg.com/640/480/people',
-            },
-            {
-              '768w': 'https://placeimg.com/768/576/people',
-            },
-          ],
-        },
-        {
-          uid: '6',
-          title: 'Title',
-          subtext: 'Subtitle',
-          description: 'Ausstellung',
-          additional: '07.05.2020 - 21.05.2020',
-          imageUrl: 'https://placeimg.com/641/480/arch',
-          href: 'http://base.uni-ak.ac.at',
-          previews: [
-            {
-              '460w': 'https://placeimg.com/461/341/arch',
-            },
-            {
-              '640w': 'https://placeimg.com/641/480/arch',
-            },
-            {
-              '768w': 'https://placeimg.com/769/576/arch',
-            },
-          ],
-        },
-      ],
+      carouselData: [],
     };
   },
   computed: {
