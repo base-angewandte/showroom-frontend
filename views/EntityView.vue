a1ec8f5cf5b82f610ec4d4ba22385faa68bada5a
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2022-01-20 16:25:45 +0100

    feat: fix edit functionality for additional entities

--- views/EntityView.vue
+++ views/EntityView.vue
@@ -105,8 +105,8 @@ export default {
     },
     userCanEdit() {
       return this.entryId.includes(this.userId)
-        || this.userEditPermissions.includes(this.entryId)
-        || this.userEditPermissions.includes(this.entryId.split('-').pop());
+        || (this.userEditPermissions && this.userEditPermissions.includes(this.entryId))
+        || (this.userEditPermissions && this.userEditPermissions.includes(this.entryId.split('-').pop()));
     },
   },
 };
