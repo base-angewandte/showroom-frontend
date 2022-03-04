df05d0439f19806b556fbd228953081cde971adb
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2022-03-04 13:57:27 +0100

    fix: #1752: showcase: add missing prev/next functionality on mobile view to carouselOptions

--- components/Edit/Showcase.vue
+++ components/Edit/Showcase.vue
@@ -750,6 +750,10 @@ export default {
           el: '.swiper-pagination',
           clickable: true,
         },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
         breakpoints: {
           640: {
             slidesPerView: 2,
