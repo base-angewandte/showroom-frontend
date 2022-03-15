856c374be5b5b6eb0312ed6cecde18ca9e33c9dc
bsaltuari <bernhard.saltuari@uni-ak.ac.at>
2022-03-15 17:07:58 +0100

    feat: #1759: add missing app-prefix to next parameter for login url

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,6 @@
 export default function auth({ store, redirect, route }) {
   // If the user is not authenticated
   if (process.env.authRequired && !store.getters['appData/getAuthState']) {
-    return redirect(`${process.env.backendBaseUrl}${process.env.backendPrefix}/accounts/login/?next=${route.path}`);
+    return redirect(`${process.env.backendBaseUrl}${process.env.backendPrefix}/accounts/login/?next=${process.env.appPrefix}/${route.path}`);
   }
 }
